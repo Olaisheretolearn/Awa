90dda84d9bc70afc309ed5be123fa5e5afaa46d5
Olaoluwa Oke <63797098+Olaisheretolearn@users.noreply.github.com>
2025-06-27 00:03:15 -0500

    haha

--- Awa/src/main/kotlin/com/summerlockin/Awa/service/ShoppingItemService.kt
+++ Awa/src/main/kotlin/com/summerlockin/Awa/service/ShoppingItemService.kt
@@ -2,6 +2,7 @@ package com.summerlockin.Awa.service
 
 import com.summerlockin.Awa.DTO.ShoppingItemCreateRequest
 import com.summerlockin.Awa.DTO.ShoppingItemResponse
+import com.summerlockin.Awa.exception.NotFoundException
 import com.summerlockin.Awa.model.ShoppingItem
 import com.summerlockin.Awa.repository.ShoppingItemRepository
 import org.bson.types.ObjectId
@@ -13,7 +14,7 @@ class ShoppingItemService(
     private val shoppingItemRepository: ShoppingItemRepository
 ) {
 
-    fun createShoppingItem(request : ShoppingItemCreateRequest): ShoppingItemResponse {
+    fun createShoppingItem(request: ShoppingItemCreateRequest): ShoppingItemResponse {
         val shoppingItem = ShoppingItem(
             roomId = ObjectId(request.roomId),
             listName = request.listName,
@@ -21,12 +22,40 @@ class ShoppingItemService(
             addedByUserId = ObjectId(request.addedByUserId),
             addedByName = request.addedByName,
             isBought = request.isBought,
-            boughtByUserId = ObjectId(request.boughtByUserId),
-            boughtByName =  request.boughtByName,
+            boughtByUserId = request.boughtByUserId?.let { ObjectId(it) },
+            boughtByName = request.boughtByName,
             createdAt = Instant.now()
         )
         return shoppingItemRepository.save(shoppingItem).toDTO()
     }
+
+
+    fun deleteShoppingItem(itemId: String): Boolean {
+        val item = shoppingItemRepository.findById(ObjectId(itemId))
+            .orElseThrow { NotFoundException("Shopping item not found") }
+
+        shoppingItemRepository.delete(item)
+        return true
+    }
+
+    //patch a shopping item
+
+    //get all shopping items in a room
+
+    //mark as bought
+
+    // get items by either bough or unbought // essentially by status
+
+
+    //search item by name
+
+
+    // get all items added by a specific user
+
+
+    //get items bought by a sopecific user
+
+
 
     fun ShoppingItem.toDTO(): ShoppingItemResponse {
         return ShoppingItemResponse(
